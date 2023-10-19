81a8cd85a4f02232ccfb78ede99fd22ea34a9bdf
youngmikec <michaelozor15@gmail.com>
2023-10-19 13:43:37 +0100

    add filter modal section

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -24,21 +24,24 @@ export default function RootLayoutNav() {
         <Stack.Screen name="index" options={{
           header: () => <CustomHeader />,
         }} />
-        <Stack.Screen name="(modal)/filter" options={
-          {
-            presentation: 'modal',
-            headerTitle: 'Filter',
-            headerShadowVisible: false,
-            headerStyle: {
-              backgroundColor: Colors.lightGrey,
-            },
-            headerLeft: () => (
-              <TouchableOpacity onPress={() => {navigation.goBack()}}>
-                <Ionicons name='close-outline' size={28} color={Colors.primary} />
-              </TouchableOpacity>
-            )
-          }
-        } />
+        <Stack.Screen 
+          name="(modal)/filter" 
+          options={
+            {
+              presentation: 'modal',
+              headerTitle: 'Filter',
+              headerShadowVisible: false,
+              headerStyle: {
+                backgroundColor: Colors.lightGrey,
+              },
+              headerLeft: () => (
+                <TouchableOpacity onPress={() => {navigation.goBack()}}>
+                  <Ionicons name='close-outline' size={28} color={Colors.primary} />
+                </TouchableOpacity>
+              )
+            }
+          } 
+        />
       </Stack>
     </BottomSheetModalProvider>
   );
